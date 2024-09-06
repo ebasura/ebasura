ebf67c18686e9a36e76642f33c1fad3ad8de6c3f
bitress <byteress@gmail.com>
2024-09-06 20:01:42 +0800

    test

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var bitress = {
         dataTables: {}
     },
     Http: {
-        live_monitoring: 'https://ws.ebasura.online',
+        live_monitoring: 'ws://ws.ebasura.online',
         system_monitoring: 'https://api.ebasura.online'
     }
 };
